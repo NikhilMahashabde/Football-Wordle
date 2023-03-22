c44e60f67beb3fa50463800ad1810e2b24d32488
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-03-22 22:07:31 +1100

    test2

---
+++ project 1 nm.js
@@ -0,0 +1,39 @@
+
+//Dataset container - tba import from excel or JSON real list 
+let dataset = [
+    {name: "L. Messi", club: "barcelona", league: "Laliga"}, 
+    {name: "Christiano Ronaldo", club: "al-hilal", league: "EPL"}
+]
+
+
+// function to generate random name from the list of players. 
+// Optional take into account selected league
+
+let flagsLeagueSelected = {
+    "La Liga": false, 
+    "EPL": true,
+}
+
+
+// function to filter / create sublist 
+
+function filterDataset(flagsLeagueSelected, dataset) {
+
+    let selectedName = "";
+
+    return selectedName;
+}
+
+
+// select random name from filtered list
+
+let targetName = filterDataset(flagsLeagueSelected, dataset)
+
+gridLength = targetName.length;
+
+
+
+//grid container create
+    //loop over and create N lines 
+
+    //
