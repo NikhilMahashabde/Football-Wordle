8d49b1d42def02505a6185247ab7c79d2148f781
Nikhil Mahashabde <nikhil.mahashabde@gmail.com>
2023-03-23 12:34:48 +1100

    2023.03.23 added keyboard layout grid

--- project 1 nm.js
+++ project 1 nm.js
@@ -5,6 +5,42 @@ let dataset = [
     {name: "Christiano Ronaldo", club: "al-hilal", league: "EPL"}
 ]
 
+// keyboard keyset to display
+let displayKeysMap = {
+    topRow: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "del"],
+    middleRow: ["a", "s", "d", "f", "g", "h", "j", "k", "l", "enter"],
+    bottomRow: ["z", "x", "c", "v", "b", "n", "m"]
+}
+let keyboardContainerTopRowDiv = document.getElementById("keyboardContainerTopRow");
+let keyboardContainerMiddleRowDiv = document.getElementById("keyboardContainerMiddleRow");
+let keyboardContainerBottomRowDiv = document.getElementById("keyboardContainerBottomRow");
+
+displayKeyboard(displayKeysMap);
+
+function displayKeyboard(displayKeysMap){
+    displayKeysMap.topRow.forEach(element => {
+        let key = generateElement("div", keyboardContainerTopRowDiv)
+        key.classList.add("keyboardKeys")
+        key.textContent = element;
+        
+    });
+
+    displayKeysMap.middleRow.forEach(element => {
+        let key = generateElement("div", keyboardContainerMiddleRowDiv)
+        key.textContent = element;
+        key.classList.add("keyboardKeys")
+    });
+    displayKeysMap.bottomRow.forEach(element => {
+        let key = generateElement("div", keyboardContainerBottomRowDiv)
+        key.textContent = element;
+        key.classList.add("keyboardKeys")
+    });
+    
+
+}
+
+
+
 
 // function to generate random name from the list of players. 
 // Optional take into account selected league
@@ -25,7 +61,7 @@ let difficultMap = {
 
 function filterDataset(flagsLeagueSelected, dataset) {
 
-    let selectedName = "Messi";
+    let selectedName = "Christiano Ronaldo";
 
     return selectedName;
 }
@@ -39,18 +75,15 @@ let gridLength = targetName.length;
 
 
 
-
-
-
 //grid container create
-let guessContainer = document.getElementById("GuessContainer");    
-guessContainer.style.gridTemplateColumns = `repeat(${gridLength}, 100px)`;
+let containerLetterOutput = document.getElementById("containerLetterOutput");    
+containerLetterOutput.style.gridTemplateColumns = `repeat(${gridLength}, 100px)`;
 
 //loop over and create N lines 
 
 for (j=0; j<difficultMap[currentDifficulty]; j++){
     for (let i =0; i<gridLength; i++){
-        let newNode = generateElement("input", guessContainer, 'id="sadgasg"', 'class="hi"');
+        let newNode = generateElement("div", containerLetterOutput, 'id="sadgasg"', 'class="hi"');
         newNode.classList.add("charInputBox");
         newNode.maxLength = 1;
         newNode.id = `0.${i}`;
